7f3ba86e2505cb8c3caca455d5dad43bb0ce1a74
Jack Wu <bbonline229@gmail.com>
2019-07-13 12:08:28 +0800

    新增 MovieDetail Cell

---
+++ MovieCollection/MovieCollection/Views/Cells/MovieDetailCell.swift
@@ -0,0 +1,21 @@
+//
+//  MovieDetailCell.swift
+//  MovieCollection
+//
+//  Created by Jack on 7/13/19.
+//  Copyright © 2019 Jack. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailCell: UICollectionViewCell {
+
+    @IBOutlet weak var movieNameLabel: UILabel!
+    @IBOutlet weak var movieImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
